f87ba120984f92725eca0607ed1e08258f9c0c89
Due <kcy7th@naver.com>
2025-01-21 09:46:26 +0900

    오디오매니저

--- Assets/Script/AudioManager.cs
+++ Assets/Script/AudioManager.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static AudioManager Instance;
+    
+    public AudioSource audioSource;
+    public AudioClip clip;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+    }
+    
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
         
