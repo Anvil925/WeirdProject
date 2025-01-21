a86cd20d0ba4d62ad19c0d5b5528a16e1ad35656
subin <subin24890@gmail.com>
2025-01-21 19:19:27 +0900

    게임 매니져 레벨별 시간 분배

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -4,19 +4,64 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
+
+    public Card firstCard;
+    public Card secondCard;
+
+    public Text timeTxt;
+
+    public GameObject endTxt;
+
     public AudioSource audioSource;
     public AudioClip clip;
+
+    public int cardCount = 0;
+    public int level = 1;
+
+    float time;
+    float endtime = 0f;
+
+    public void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+    }
 
     void Start()
     {
         Time.timeScale = 1.0f;
         audioSource = GetComponent<AudioSource>();
+
+        if (level == 1)
+        {
+            time = 300.0f;
+        }
+        else if(level == 2) 
+        {
+            time = 180.0f;
+        }
+        else if(level == 3)
+        {
+            time = 60.0f;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        time += Time.deltaTime;
+        timeTxt.text = time.ToString("N2");
+
+
+        if (time >= endtime)
+        {
+            endTxt.SetActive(true);
+            Time.timeScale = 0f;
+        }
     }
     
 
