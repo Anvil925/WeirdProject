bbaeb11b4d00e8555226cdf60ccd520f8b813da9
Due <kcy7th@naver.com>
2025-01-21 22:03:00 +0900

    w

--- Assets/Script/AudioManager.cs
+++ Assets/Script/AudioManager.cs
@@ -5,22 +5,21 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     
+    public static AudioManager Instance;
+    
     public AudioSource audioSource;
     public AudioClip clip;
 
     private void Awake()
     {
+    
         
     }
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
-
+        
         audioSource.clip = this.clip;
         audioSource.Play();
-    }
-    void Update()
-    {
-        
     }
 }
