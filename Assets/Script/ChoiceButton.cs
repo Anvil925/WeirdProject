044dca5c24f080aadcf2445c8e79e623ec1bc4f8
subin <subin24890@gmail.com>
2025-01-21 14:23:41 +0900

    버튼 세팅

--- Assets/Script/ChoiceButton.cs
+++ Assets/Script/ChoiceButton.cs
@@ -6,16 +6,21 @@ using UnityEngine.SceneManagement;
 public class ChoiceButton : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Choice2Lv()
+    public void Choice2Lv()
         //2lv
         //메인신에서 시작 타이머 3분 으로 변경하기
     {
 
     }
-    void Choice3Lv()
+    public void Choice3Lv()
         //3lv
         //메인신에서 시작 타이머 1분 으로 변경하기
     {
 
+    }
+
+    public void BackOpenS()
+    {
+        SceneManager.LoadScene("OpenScene");
     }
 }
