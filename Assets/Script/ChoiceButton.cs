67a0b431ce27ede74193e82569de81ec8f74bce2
subin <subin24890@gmail.com>
2025-01-21 10:54:30 +0900

    초이스스스크립트 와 버튼 이미지

---
+++ Assets/Script/ChoiceButton.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChoiceButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Choice()
+    {
+        if()
+            //2단계
+        {
+            SceneManager.LoadScene("MainScene");
+
+        }
+        else if ()
+            //3단계
+        {
+            SceneManager.LoadScene("MainScene");
+
+        }
+        
+    }
+}
