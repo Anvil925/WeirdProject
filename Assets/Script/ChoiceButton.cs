08b90bb931440269027af2c173efb7f739502701
subin <subin24890@gmail.com>
2025-01-21 11:23:47 +0900

    Update ChoiceButton.cs

--- Assets/Script/ChoiceButton.cs
+++ Assets/Script/ChoiceButton.cs
@@ -8,22 +8,22 @@ public class ChoiceButton : MonoBehaviour
     // Start is called before the first frame update
     void Choice()
     {
-        /*
-        if(toplevel <= 2)
+        
+        if (PlayerPrefs.GetFloat("toplevel") <= 2)
             //2단계
         {
             SceneManager.LoadScene("MainScene");
             time = 180.0f;
             // 메인 씬 남은 시간 3분으로 변경
         }
-        else if (toplevel <= 3)
+        else if (PlayerPrefs.GetFloat("toplevel") <= 3)
             //3단계
         {
             SceneManager.LoadScene("MainScene");
             time = 60.0f;
             // 메인 씬 남은 시간 1분으로 변경
 
-        }*/
+        }
 
     }
 }
