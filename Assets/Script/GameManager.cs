526f0be5195c18271396e41f039700640f02b2d5
Due <kcy7th@naver.com>
2025-01-21 11:39:17 +0900

    게임매니저

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip clip;
+    
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
