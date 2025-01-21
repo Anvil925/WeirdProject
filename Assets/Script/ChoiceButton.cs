657996d2e0c5a4f05cf1fffd588378cc93b10cd9
subin <subin24890@gmail.com>
2025-01-21 11:29:14 +0900

    테스트 게임 매니져 생성

--- Assets/Script/ChoiceButton.cs
+++ Assets/Script/ChoiceButton.cs
@@ -13,14 +13,12 @@ public class ChoiceButton : MonoBehaviour
             //2단계
         {
             SceneManager.LoadScene("MainScene");
-            time = 180.0f;
-            // 메인 씬 남은 시간 3분으로 변경
+
         }
         else if (PlayerPrefs.GetFloat("toplevel") <= 3)
             //3단계
         {
             SceneManager.LoadScene("MainScene");
-            time = 60.0f;
             // 메인 씬 남은 시간 1분으로 변경
 
         }
