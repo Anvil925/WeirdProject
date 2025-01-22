0fc76012842ec6a049f2401f549a39a57d50f2d0
Anvil925 <anvil_925@naver.com>
2025-01-22 09:27:12 +0900

    씬로더 스크립트 수정
    간소화 및 오류 해결완료 이상없음

--- Assets/Script/SceneLoader.cs
+++ Assets/Script/SceneLoader.cs
@@ -7,16 +7,8 @@ public class SceneLoader : MonoBehaviour
 {
     public Animator transition;
     public float transitionTime = 1f;
-
     // 버튼 연결
     public Button loadSceneButton;
-
-    private void Start()
-    {
-        // 버튼 클릭 이벤트 등록
-        loadSceneButton.onClick.AddListener(LoadNextLevel);
-    }
-
     public void LoadNextLevel()
     {
         StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
