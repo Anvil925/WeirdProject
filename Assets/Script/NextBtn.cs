b74ae14d2b1c6ef2b6ee7704af3cdee6f20e92f8
subin <subin24890@gmail.com>
2025-01-23 21:11:37 +0900

    1.1

--- Assets/Script/NextBtn.cs
+++ Assets/Script/NextBtn.cs
@@ -5,32 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class NextBtn : MonoBehaviour
 {
-    int level;
+
 
 
     public void Start()
     {
-        level = PlayerPrefs.GetInt("GameLv");
+
     }
     public void Next2Lv()
     {
-        if (level == 2)
-        {
-            PlayerPrefs.DeleteKey("Loadlv2");
             PlayerPrefs.SetString("Loadlv2", "2");
             PlayerPrefs.Save();
             SceneManager.LoadScene("Main2Scene");
-        }
     }
 
     public void Next3Lv()
     {
-        if (level == 3)
-        {
-            PlayerPrefs.DeleteKey("Loadlv3");
             PlayerPrefs.SetString("Loadlv3", "3");
             PlayerPrefs.Save();
-            SceneManager.LoadScene("Main2Scene");
-        }
+            SceneManager.LoadScene("Main3Scene");
+
     }
 }
